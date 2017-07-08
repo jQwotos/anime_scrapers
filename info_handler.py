a75c76e13c5ec7521c211a8217717406fef591f1
Soham Sen <sohamsen1000@gmail.com>
2017-07-08 19:06:18 +0530

    Fixed info collection (#1)

--- info_handler.py
+++ info_handler.py
@@ -1,27 +1,29 @@
 import glob
 
-# from .templates.module_search import ModuleSearch
-from . import templates.module_search.ModuleSearch
+from .templates.module_search import ModuleSearch
 
 
 class InfoHandler(ModuleSearch):
 
     def __init__(self):
-        self.info_collector_location = "info_collectors/"
-        self.modules = glob.glob("%s*.py" % (self.info_collector_location))
-        self.modules = self._load_modules()
+        self._get_modules('info_collectors')
 
     def _search_module(self, query, strict, module):
         return module.search(query, strict)
 
     def search(self, query, strict=False):
-        return [self._search_module(query, strict, x) for x in self.modules]
+        return [
+            self._search_module(query, strict, x)
+            for x in self.modules
+        ]
 
     def _details_module(self, id, module):
         return module.getDetailedInfo(id)
 
     def getDetailedInfo(self, id):
-        return [self._details_module(id, x) for x in self.modules]
+        return [
+            self._details_module(id, x) for x in self.modules
+        ]
 
 
 info_handler = InfoHandler()
