535c6520271924c8ee4098928b9cee6be9468afc
FadedCoder <sohamsen1000@gmail.com>
2017-07-13 16:49:11 +0530

    Fixed code (removed unzipping)

--- init_search.py
+++ init_search.py
@@ -1,13 +1,10 @@
 from datetime import date
 import requests
-import gzip
-import shutil
 
 
 INFO_FILE = "last_download.txt"
 DOWNLOAD_URL = "http://anidb.net/api/anime-titles.xml.gz"
-DOWNLOAD_FILE = "anime-titles.xml.gz"
-UNZIPPED_FILE = "anime-titles.xml"
+DOWNLOAD_FILE = "anime-titles.xml"
 
 
 class DownloadList:
@@ -43,16 +40,10 @@ class DownloadList:
                 if chunk:
                     f.write(chunk)
 
-    def untar_list(self):
-        with gzip.open(DOWNLOAD_FILE, 'rb') as f_in, \
-             open(UNZIPPED_FILE, 'wb') as f_out:
-            shutil.copyfileobj(f_in, f_out)
-
     def get_file(self):
         if self.need_to_download():
             self.write_today_ordinal()
             self.download_list()
-            self.untar_list()
 
 
 download_list = DownloadList()
